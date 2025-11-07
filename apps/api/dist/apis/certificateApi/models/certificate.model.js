ad45fc592968e5b772afe8a218db164c8527d6c6
Shivraj <patilshivraj3008@gmail.com>
2025-11-07 12:01:06 +0530

    package issue solved

--- apps/api/dist/apis/certificateApi/models/certificate.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const certificateSchema = new mongoose_1.Schema({
-    title: { type: String, required: true },
-    imageUrl: { type: String, required: true }, // Multer saves the file, we save path
-    addedBy: { type: mongoose_1.Schema.Types.ObjectId, ref: "User", required: true },
-}, { timestamps: true });
-exports.default = (0, mongoose_1.model)("Certificate", certificateSchema);
